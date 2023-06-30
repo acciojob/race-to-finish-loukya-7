fe793b4bce9caeeab198922301d263840645dc16
loukya-7 <131159563+loukya-7@users.noreply.github.com>
2023-06-30 20:19:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
+
 window.promises = [];
+
+// Function to generate a promise that resolves after a random time
+function generatePromise(min, max) {
+  const delay = Math.floor(Math.random() * (max - min + 1) + min);
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(`Promise resolved after ${delay} seconds`);
+    }, delay * 1000);
+  });
+}
+
+// Add your promises to the array
+window.promises.push(generatePromise(1, 5));
+window.promises.push(generatePromise(1, 5));
+window.promises.push(generatePromise(1, 5));
+window.promises.push(generatePromise(1, 5));
+window.promises.push(generatePromise(1, 5));
+
 
 // Do not change the code above this
 // add your promises to the array `promises`
